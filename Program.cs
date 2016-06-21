de36fa2b6d4ff4fe0ebd26eecf6c131b269b4a6e
hmoriya <hmoriya@archway.co.jp>
2016-06-21 13:54:00 +0900

    update project.json settings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleApplication
 {
@@ -7,6 +9,23 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
+    public class TodoDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./Todo.db");
+        }
+        public DbSet<ToDo> Todos { get; set; }
+    }
+    [Table("Todos")]
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public bool IsDone { get; set; }
+        public string Text { get; set; }
+    }
 }
+
