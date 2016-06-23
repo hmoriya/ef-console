946ae46849f72451828d03ccce8006348e014f58
Naoto Okuda <nokuda@archway.co.jp>
2016-06-23 09:14:12 +0900

    add repository pattern and constructor injection.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -18,6 +21,7 @@ namespace ConsoleApplication
                                         //options.UseSqlite("Filename=./Todo.db");
                                     });
 
+            serviceCollection.AddTransient<TodoRepository, TodoRepository>();
             return serviceCollection.BuildServiceProvider();
         }
 
@@ -30,7 +34,7 @@ namespace ConsoleApplication
             // TODO get DbContext instance from DIContainer.
             using(var db = provider.GetService<TodoDbContext>())
             {
-                db.Todos.Add(new ToDo(){ IsDone = false,Text = "First Commit"});
+                db.Todos.Add(new Todo(){ IsDone = false,Text = "First Commit"});
                 
                 db.SaveChanges();
 
@@ -38,11 +42,18 @@ namespace ConsoleApplication
                 {
                     Console.WriteLine($"[{item.Id}] {item.Text} : {item.IsDone}");
                 }
+            }
 
+            // TODO repository get by DIContainer.
+            using(var repos = provider.GetService<TodoRepository>())
+            {
+                repos.Add(new Todo() {IsDone = false, Text = "Added by repostitory" });
+                repos.Save();
             }
 
         }
     }
+
     public class TodoDbContext : DbContext
     {
 
@@ -61,14 +72,95 @@ namespace ConsoleApplication
                 optionsBuilder.UseSqlite("Filename=./Todo.db");
             }
         }
-        public DbSet<ToDo> Todos { get; set; }
+        public DbSet<Todo> Todos { get; set; }
     }
+
     [Table("Todos")]
-    public class ToDo
+    public class Todo
     {
         public int Id { get; set; }
         public bool IsDone { get; set; }
         public string Text { get; set; }
     }
+
+    public class TodoRepository : Repository<TodoDbContext, Todo>
+    {
+        public TodoRepository(TodoDbContext context) : base(context) 
+        {
+        }
+    }
+
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity Find(params object[] keyValues);
+
+        IList<TEntity> ListAll();
+
+        IList<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+
+        void Save();
+    }
+
+    public abstract class Repository<TContext, TEntity> : IRepository<TEntity>
+            where TContext : DbContext, new()
+            where TEntity : class
+    {
+
+        protected Repository(TContext context) 
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+        }
+        protected Repository() : this(new TContext())
+        {
+        }
+
+        private readonly DbContext context;
+        private readonly DbSet<TEntity> dbSet;
+
+        public TEntity Find(params object[] keyValues)
+        {
+            // Find method not implementd EF Core rc2
+            throw new NotImplementedException();
+            // return dbSet.Find(keyValues);
+        }
+
+        public IList<TEntity> ListAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public IList<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.Where(predicate).ToList();
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Dispose() 
+        {
+            if (context != null) 
+            {
+                context.Dispose();
+            }
+        }
+    }
+
 }
 
